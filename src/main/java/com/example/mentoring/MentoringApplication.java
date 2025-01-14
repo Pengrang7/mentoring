e5ae8a825a2f1589e0545262299597114ae36c17
pengrang7 <jeeyen77@gmail.com>
2025-01-14 15:47:15 +0900

    1주차 과제

--- src/main/java/com/example/mentoring/MentoringApplication.java
+++ src/main/java/com/example/mentoring/MentoringApplication.java
@@ -2,8 +2,14 @@ package com.example.mentoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.mentoring")
+@EntityScan("com.example.mentoring.entity")
+@EnableJpaRepositories("com.example.mentoring.repository")
 public class MentoringApplication {
 
 	public static void main(String[] args) {
